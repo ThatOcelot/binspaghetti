8a2210bedd2f210aab385c8e2e9529c320e1e955
ThatOcelot <62442404+ThatOcelot@users.noreply.github.com>
2021-08-25 21:37:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #Allows you to select whether you want to convert from binary to decimal or from decimal to binary
 conv = input("Do you want to convert to binary or decimal? ")
 
-if (conv == "decimal" or "Decimal"):
 #Used for converting to decimal
+if (conv == "decimal" or "Decimal"):
 	bin_num = input("Enter a binary number: ")
 	try:
 		dec_num = int(bin_num, 2)
@@ -10,13 +10,12 @@ if (conv == "decimal" or "Decimal"):
 		print("Done ✓")
 	except ValueError:
 		print("Invalid binary number")
-elif (conv == "binary" or "Binary"):
 #Used for converting to binary
+elif (conv == "binary" or "Binary"):
 	dec_num = int(input("Enter a decimal number: "))
 	bin_num = bin(dec_num)
 	print("The binary value is: " + str(bin_num))
 	print("Done ✓")
-else:
 #Error message
+else:
         print("Error: Invalid Input")
-
