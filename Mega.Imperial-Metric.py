97ed6cc5cea4e34e78c99e19cce2d4a4d1aaf875
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-08-26 14:14:36 -0700

    Create Mega.Imperial-Metric.py

---
+++ Mega.Imperial-Metric.py
@@ -0,0 +1,16 @@
+Units= input("what System would you like to use Metric or Imperial: ")
+
+if (Units == Imperial)
+
+elif (Units == Metric)
+    MetU=input("What unit would you like to convert")
+    
+    if (MetU == Kilograms)
+            Kilo=int(input("How many Kilos:"))
+           try:
+               print("pounds"+kilo*0.454)
+    elif
+else:
+
+   print("Error: Invalid_SystemNotFound")
+     
