f5873b87cb623c1749bb0c38c9ec35ae3a8d734b
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-08-30 13:46:53 -0700

    Update and rename speedcounter.py to NumCruncher.py
    enter a value and it will out put all numbers lower than it 
    say a 4 digit combination it will out put every number upto 9999

---
+++ NumCruncher.py
@@ -0,0 +1,10 @@
+sol=int(input("Enter your number: "))
+num=int("1")
+num2=int("10")
+num3=(num*num2)
+while sol>=num3:
+  num3=(num*num2)
+  num2=(num+num3)
+  print(num3-1)
+
+##very speedy
--- speedcounter.py
+++
@@ -1,8 +0,0 @@
-num=int("1")
-num2=int("10")
-while 100>=1:
-  num3=(num*num2)
-  num2=(num+num3)
-  print(num3)
-
-##its very fast
