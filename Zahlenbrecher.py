ba50d19681e8658c9528323e2feee790e7604da4
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-08-30 14:56:04 -0700

    Update Zahlenbrecher.py
    now counts from 1 up

--- Zahlenbrecher.py
+++ Zahlenbrecher.py
@@ -1,6 +1,6 @@
 sol=int(input("Enter your number: "))
 num=int("1")
-num2=int("10")
+num2=int("2")
 num3=(num*num2)
 while sol>=num3:
   num3=(num*num2)
@@ -9,3 +9,4 @@ while sol>=num3:
 
   
 ##very speedy
+
