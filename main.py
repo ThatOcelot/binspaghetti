e5e5325d3509c450d75109a4cd4152d47e6b33e6
ThatOcelot <62442404+ThatOcelot@users.noreply.github.com>
2021-08-31 10:18:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-#Allows you to select whether you want to convert from binary to decimal or from decimal to binary
-conv = input("Do you want to convert to binary or decimal? ")
+conv1 = input("Do you want to convert from binary, hexadecimal, or decimal? ")
 
-#Used for converting to decimal
-if (conv == "decimal"):
+if (conv1 == "binary"):
 	bin_num = input("Enter a binary number: ")
-	try:
-		dec_num = int(bin_num, 2)
-		print("The decimal value is: " + str(dec_num))
-		print("Done ✓")
-	except ValueError:
-		print("Invalid binary number")
-#Used for converting to binary
-elif (conv == "binary"):
+	dec_num = int(bin_num, 2)
+	conv2 = input("Do you want to convert to hexadecimal or decimal? ")
+	if (conv2 == "hexadecimal"):
+		hex_num = hex(dec_num)
+		print(hex_num)
+	elif (conv2 == "decimal"):
+		print(dec_num)
+	else:
+		print("Error: Invalid Input")
+elif (conv1 == "decimal"):
 	dec_num = int(input("Enter a decimal number: "))
-	bin_num = bin(dec_num)
-	print("The binary value is: " + str(bin_num))
-	print("Done ✓")
-#Error message
+	conv2 = input("Do you want to convert to hexadecimal or binary? ")
+	if (conv2 == "hexadecimal"):
+		hex_num = hex(dec_num)
+		print(hex_num)
+	elif (conv2 == "binary"):
+		bin_num = bin(dec_num)
+		print(bin_num)
+	else:
+		print("Error: Invalid Input")
+elif (conv1 == "hexadecimal"):
+	hex_num = input("Enter a hexadecimal number: ")
+	dec_num = int(hex_num, 16)
+	conv2 = input("Do you want to convert to decimal or binary? ")
+	if (conv2 == "decimal"):
+		print(dec_num)
+	elif (conv2 == "binary"):
+		bin_num = bin(dec_num)
+		print(bin_num)
+	else:
+		print("Error: Invalid Input")
 else:
-        print("Error: Invalid_Input")
+	print("Error: Invalid Input")
