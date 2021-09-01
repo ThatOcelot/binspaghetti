7890c14e90c61e76486b5814edfecd1d34f9b4c8
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-09-01 14:05:06 -0700

    Update Zahlenbrecher.py

--- Zahlenbrecher.py
+++ Zahlenbrecher.py
@@ -1,12 +1,10 @@
-sol=int(input("Enter your number: "))
-num=int("1")
-num2=int("1")
-num3=(num*num2)
-while sol>=num3:
-  num3=(num*num2)
-  num2=(num+num3)
-  print(num3-1)
-
-  
-##very speedy 
-
+sol = int(input("Enter your number: "))
+num = int("1")
+num2 = int("1")
+num3 = (num * num2)
+print("Computing....")
+while sol >= num3:
+    num3 = (num * num2)
+    num2 = (num + num3)
+    print(num3 - 1)
+##very speedy
