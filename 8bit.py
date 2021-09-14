ea2bce01979e63581976c84bf49676403b7cbb41
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-09-14 10:15:34 -0700

    Create 8bit.py
    random binary generator

---
+++ 8bit.py
@@ -0,0 +1,14 @@
+import random
+val1=(1)
+val2=(0)
+while val1>=val2:
+  a=random.randint(0,1)
+  b=random.randint(0,1)
+  c=random.randint(0,1)
+  d=random.randint(0,1)
+  e=random.randint(0,1)
+  f=random.randint(0,1)
+  g=random.randint(0,1)
+  h=random.randint(0,1)
+  binary=(str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g)+str(h))
+  print(binary)
