50edf730003bc713189d4bc9dfb63be9f9d78c4d
SetatX <89603580+Semtanx@users.noreply.github.com>
2021-09-15 14:59:19 -0700

    Create Mol_calculator.py

---
+++ Mol_calculator.py
@@ -0,0 +1,10 @@
+mass=float(input("Whats the atomic mass: "))
+avogadro=float(6.02214076*(10^22))
+mol=float(0.7288)
+amount=int(input("How many of these are there put 1 to skip: "))
+molrat=(mass/mol)
+grams=(mass*avogadro)
+print("\nthe Mol ratio is: "+str(molrat*amount)+" \nit weighs: "+str(grams*amount)+" grams")
+##dont write anything
+##click run and enter the atoms mass then you get the ratio and the weight
+# ima work on it in a seprate file  going to make it better
